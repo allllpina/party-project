a7ae011809c19dacc5505854799fab9caa714c79
Danila <danilcrystal8@gmail.com>
2021-08-26 13:43:07 +0300

    second

--- src/components/middle-section/index.js
+++ src/components/middle-section/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./style.css";
+import photo1 from "./imgs/pic-html-on-page.jpg";
 
 export function MiddleSection() {
   return (
     <section id="cards-section">
       <div className="info-card">
+        <img src={photo1} alt="photo" className="cardPhoto"></img>
+        <h1>WEB Developers</h1>
         <button>More</button>
       </div>
     </section>
