11e485ec61a6ea24247deaa3cc60cfd34ec0a267
Danila <danilcrystal8@gmail.com>
2021-08-26 15:22:59 +0300

    parallax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import { MiddleSection } from "./components/middle-section";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener(handleScroll);
+  }, []);
   return (
     <div className="App">
       <section id="tittle-section">
-        <h1 className="tittle">
+        <h1
+          className="tittle"
+          style={{ transform: `translateX(${offsetY * 0.3}%)` }}
+        >
           Hello <br /> World!
         </h1>
       </section>
