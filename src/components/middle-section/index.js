ce4f974607e82176ceead5e1c6ae29628b3ef54c
Danila <danilcrystal8@gmail.com>
2021-08-27 11:09:22 +0300

    AOS

--- src/components/middle-section/index.js
+++ src/components/middle-section/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./style.css";
 import photo1 from "./imgs/pic-html-on-page.jpg";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export function MiddleSection() {
+  Aos.init({ duration: 1500 });
   return (
     <section id="cards-section">
-      <div className="info-card">
+      <div className="info-card" data-aos="flip-left">
+        <img src={photo1} alt="photo" className="cardPhoto"></img>
+        <h1>WEB Developers</h1>
+        <button>More</button>
+      </div>
+      <div className="info-card" data-aos="fade-left">
+        <img src={photo1} alt="photo" className="cardPhoto"></img>
+        <h1>WEB Developers</h1>
+        <button>More</button>
+      </div>
+      <div className="info-card" data-aos="fade-left">
+        <img src={photo1} alt="photo" className="cardPhoto"></img>
+        <h1>WEB Developers</h1>
+        <button>More</button>
+      </div>
+      <div className="info-card" data-aos="fade-left">
         <img src={photo1} alt="photo" className="cardPhoto"></img>
         <h1>WEB Developers</h1>
         <button>More</button>
