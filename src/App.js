5c5e4054398b96572afda45f4290ea4e5564f749
Danila <danilcrystal8@gmail.com>
2021-08-27 10:52:23 +0300

    preloader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import "./App.css";
 import { MiddleSection } from "./components/middle-section";
 import { useState, useEffect } from "react";
+import { Preloader } from "./components/preloader";
 
 function App() {
+  const [globalDistplay, setGlobalDisplay] = useState("none");
+  const [preloaderDistplay, setPreloaderDisplay] = useState("flex");
+  setTimeout(() => {
+    setPreloaderDisplay("none");
+    setGlobalDisplay("block");
+  }, 2000);
+
   const [offsetY, setOffsetY] = useState(0);
   const handleScroll = () => setOffsetY(window.pageYOffset);
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener(handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
     <div className="App">
-      <section id="tittle-section">
-        <h1
-          className="tittle"
-          style={{ transform: `translateX(${offsetY * 0.3}%)` }}
-        >
-          Hello <br /> World!
-        </h1>
+      <div className="container" style={{ display: `${preloaderDistplay}` }}>
+        <Preloader />
+      </div>
+      <section id="global" style={{ display: `${globalDistplay}` }}>
+        <section id="tittle-section">
+          <h1
+            className="tittle"
+            style={{ transform: `translateX(${offsetY * 0.3}%)` }}
+          >
+            Hello <br /> World!
+          </h1>
+        </section>
+        <MiddleSection />
+        <section id="info-section"></section>
       </section>
-      <MiddleSection />
-      <section id="info-section"></section>
     </div>
   );
 }
---
+++ src/components/preloader/index.js
@@ -0,0 +1,19 @@
+import { React } from "react";
+import "./style.css";
+
+export function Preloader() {
+  return (
+    <div class="loadingio-spinner-double-ring-rskbkg213z">
+      <div class="ldio-4a81rd83h6u">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
