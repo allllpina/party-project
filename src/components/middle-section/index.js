ce552b4fd77ef81906b2be7327632835629308a8
Danila <danilcrystal8@gmail.com>
2021-08-27 14:53:49 +0300

    fixed

--- src/components/middle-section/index.js
+++ src/components/middle-section/index.js
@@ -8,7 +8,7 @@ export function MiddleSection() {
   Aos.init({ duration: 1500 });
   return (
     <section id="cards-section">
-      <div className="info-card" data-aos="flip-left">
+      <div className="info-card" data-aos="fade-left">
         <img src={photo1} alt="photo" className="cardPhoto"></img>
         <h1>WEB Developers</h1>
         <button>More</button>
--- src/components/preloader/index.js
+++ src/components/preloader/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 export function Preloader() {
   return (
-    <div class="loadingio-spinner-double-ring-rskbkg213z">
-      <div class="ldio-4a81rd83h6u">
+    <div className="loadingio-spinner-double-ring-rskbkg213z">
+      <div className="ldio-4a81rd83h6u">
         <div></div>
         <div></div>
         <div>
